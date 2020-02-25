483a9248b92cbeaa151944335aaedad4eeca6313
drumnistnakano <syxqh349ysykn@gmail.com>
2020-02-25 10:23:57 +0000

    レビューCURDの機能テスト

--- tests/Feature/ReviewController/createTest.php
+++ tests/Feature/ReviewController/createTest.php
@@ -10,32 +10,51 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\User;
 use App\Review;
 
-class ReviewControllerTest extends TestCase
+class ReviewCreateTest extends TestCase
 {
     use DatabaseTransactions;
     
     private $attributes;
-    
-    /**
-     * レビュー投稿処理：正常系
-     *
-     * @return void
-     */
-    public function testCreateReview()
+    private $user;
+
+    public function setUp(): void
     {
-        $user = factory(User::class)->create();
-        $this->actingAs($user);
+        parent::setUp();
+        
+        $this->user = factory(User::class)->create();
+        $this->actingAs($this->user);
         
         $this->attributes = [
             'title' => 'ほげほげ',
             'category_id' => 1,
             'body' => 'ほげほげです。テストです。',
         ];
-        
+    }
+    
+    /**
+     * レビュー投稿処理：正常系
+     *
+     * @return void
+     */
+    public function testNormalCreate()
+    {
+        // レビュー投稿
         $response = $this->post('/review/save', $this->attributes);
 
         // リダイレクトされるか
         $response->assertStatus(302)->assertRedirect('/');
+        
+        /**
+        * 画面観点
+        */
+        // ホーム画面に登録したデータが存在するか
+        $this->get('/')
+            ->assertSee($this->attributes['title'])
+            ->assertSee($this->attributes['body']);
+        
+        /**
+        *　データ観点
+        */
         // DBに登録されているか
         $this->assertDatabaseHas('reviews', $this->attributes);
     }
@@ -45,7 +64,7 @@ class ReviewControllerTest extends TestCase
      *
      * @return void
      */
-    // public function testCreateReview()
+    // public function testAbnormalCreate()
     // {
 
     // }
