85274596e675bd6149d81ccae4d625a3c98de027
drumnistnakano <syxqh349ysykn@gmail.com>
2020-02-26 11:44:05 +0000

    CircleCiの設定ファイル更新9

--- config/database.php
+++ config/database.php
@@ -95,7 +95,7 @@ return [
         ],
 
         // CircleCI テスト用
-        'circle_testing' => [
+        'circleci' => [
             'driver' => 'mysql',
             'host' => '127.0.0.1',
             'port' => '3306',
